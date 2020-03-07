5eb836e515f07e5c6a4bba61bb66ed6aa4e27991
Petriczech <petriczech@users.noreply.github.com>
2020-03-07 22:57:52 +0100

    basic colors

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  public count = 0;
 
   constructor() {}
 
+  ngOnInit() {
+
+  }
+
+  private get getCount() {
+    return this.count;
+  }
+
+  public get bgColor() {
+    const count = this.getCount;
+    if (count <= 10) {
+      return 'bg1';
+    } else if (count > 10 && count <= 20) {
+      return 'bg2';
+    } else if (count > 20 && count <= 30) {
+      return 'bg3';
+    } else if (count > 30 && count <= 40) {
+      return 'bg4';
+    } else if (count > 40 && count <= 50) {
+      return 'bg5';
+    } else if (count > 50 && count <= 60) {
+      return 'bg6';
+    } else {
+      return 'bg7';
+    }
+  }
+
+  public isClicked() {
+    this.count++;
+  }
 }
